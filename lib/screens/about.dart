8fdb66998c266eb57db01afa98a5129f2a77a445
kevinalexandersurjadi <gt_kevin_alexander@hotmail.com>
2019-09-08 23:20:36 +0700

    About Screen

--- lib/screens/about.dart
+++ lib/screens/about.dart
@@ -1,100 +1,88 @@
 import 'package:flutter/material.dart';
+import 'package:santika_chatbot_v2/consts/assets.dart';
 import 'package:santika_chatbot_v2/consts/color.dart';
+import 'package:santika_chatbot_v2/widgets/appbar.dart';
 
 class AboutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'About This App',
-          style: TextStyle(
-            fontFamily: 'Montserrat'
-          ),
-        ),
-        backgroundColor: BershcaColors.greenHarmony,
-        elevation: 0.0,
+      appBar: BershcaAppbar(
+        title: 'Bershca Chat Room',
+        canBack: true,
       ),
-      body: Container(
-        color: BershcaColors.greenHarmony,
-        padding: EdgeInsets.all(16.0),
-        child: Stack(
-          children: <Widget>[
-            Container(
-              padding: EdgeInsets.all(32.0),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.all(Radius.circular(8.0))
+      body: SafeArea(
+        top: false,
+        bottom: true,
+        child: Container(
+          padding: EdgeInsets.all(16.0),
+          child: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.all(32.0),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Image.asset(
+                        BershcaAssets.bershcaVertical6,
+                        width: 180.0,
+                      ),
+                      SizedBox(
+                        height: 16.0,
+                      ),
+                      Text(
+                        'Bot ExpeRimental Santika Hotel Chat Assistant',
+                        style: TextStyle(
+                          fontFamily: 'Avenir',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12.0,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      SizedBox(
+                        height: 24.0,
+                      ),
+                      Text(
+                        'This chatbot app was made in collaboration between Santika Hotel and Universitas Multimedia Nusantara',
+                        style: TextStyle(
+                          fontFamily: 'Avenir',
+                          fontWeight: FontWeight.w400,
+                          fontSize: 12.0,
+                          color: Colors.black,
+                        ),
+                        textAlign: TextAlign.center,
+                      )
+                    ],
+                  ),
+                ),
               ),
-              child: Center(
-                child: Column(
+              Positioned(
+                bottom: 16.0,
+                left: 0,
+                right: 0,
+                child: Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Row(
-                      children: <Widget>[
-                        Expanded(
-                          child: Image.asset('assets/logo_umn.png'),
-                        ),
-                        Expanded(
-                          child: CircleAvatar(
-                            radius: 50.0,
-                            backgroundColor: Color.fromARGB(255, 65, 57, 55),
-                            child: Image.asset(
-                              'assets/santika_2_2.png',
-                              width: 64.0,
-                              height: 64.0,
-                            ),
-                          ),
-                        )
-                      ],
-                    ),
-                    SizedBox(
-                      height: 16.0,
-                    ),
+                    Icon(Icons.copyright, size: 12,),
+                    SizedBox(width: 8.0,),
                     Text(
-                      'Bershca',
+                      'Copyright, 2019 All Rights Reserved.',
                       style: TextStyle(
-                        fontFamily: 'Montserrat',
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24.0,
-                      ),
-                    ),
-                    Text(
-                      'Bot ExpeRimental Santika Hotel Chat Assistant',
-                      style: TextStyle(
-                        fontFamily: 'Montserrat'
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    SizedBox(
-                      height: 24.0,
-                    ),
-                    Text(
-                      'This chatbot app was made in collaboration between Santika Hotel and Universitas Multimedia Nusantara',
-                      style: TextStyle(
-                          fontFamily: 'Montserrat'
+                        fontFamily: 'Avenir',
+                        fontWeight: FontWeight.w400,
+                        fontSize: 12.0,
+                        color: Colors.black,
                       ),
                       textAlign: TextAlign.center,
                     )
                   ],
-                ),
-              ),
-            ),
-            Positioned(
-              bottom: 16.0,
-              left: 0,
-              right: 0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Icon(Icons.copyright, size: 12,),
-                  SizedBox(width: 8.0,),
-                  Text('Copyright, 2019 All Rights Reserved.',)
-                ],
+                )
               )
-            )
-          ],
+            ],
+          ),
         ),
       ),
     );
